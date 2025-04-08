06b48a84658e5d70d2506edad28908968c73e861
WingedSeal <firm09719@gmail.com>
2025-04-08 20:33:47 +0700

    feat: improve event timing

--- src/mazegame/control.py
+++ src/mazegame/control.py
@@ -12,11 +12,11 @@ class Control:
     control_event = threading.Event()
 
     def __init__(self, map: Map, game: "Game") -> None:
+        self.control_event.clear()
         self.game = game
         self.player_positions = map.get_player_positions()
 
     def _move(self, dx: int, dy: int) -> None:
-        self.control_event.wait()
         self.game.next_moves = [
             (player_pos[0], player_pos[1], dx, dy)
             for player_pos in self.player_positions
@@ -24,10 +24,26 @@ class Control:
         self.player_positions = []
         self.control_event.clear()
         self.game.game_event.set()
+        self.control_event.wait()
+
+    def halt(self) -> None:
+        self.control_event.clear()
+        self.game.game_event.set()
+        self.control_event.wait()
+
+    def _pre_run(self) -> None:
+        self.control_event.wait()
+
+    def _post_run(self) -> None:
+        self.control_event.clear()
+        self.game.game_event.set()
+        self.game.is_control_alive = False
 
     def test_run(self):
+        self._pre_run()
         self._move(-1, 0)
         self._move(-1, 0)
+        self.halt()
         self._move(-1, 0)
         self._move(0, -1)
-        self.game.is_control_alive = False
+        self._post_run()
