7e600382d78cce847dd8b2a390ca6e1aa56d2985
WingedSeal <firm09719@gmail.com>
2025-04-09 16:59:56 +0700

    docs: put solution in hard1

--- src/mazegame/api/maps/hard1.py
+++ src/mazegame/api/maps/hard1.py
@@ -23,3 +23,25 @@ HARD1 = Map(
         [Block(), Player(), Block(), Block()],
     ]
 )
+
+# def script():
+#     while get_tile(UP) != ENEMY:
+#         move(HALT)
+#     while get_tile(UP) == ENEMY:
+#         move(HALT)
+#     count = 0
+#     while count < 5:
+#         if get_tile(UP) == ENEMY:
+#             move(HALT)
+#         else:
+#             move(UP)
+#             count += 1
+#     move(RIGHT)
+#     while get_tile(LEFT) != ENEMY:
+#         move(HALT)
+#     while get_tile(LEFT) == ENEMY:
+#         move(HALT)
+#         move(LEFT)
+#     for _ in range(6):
+#         move(UP)
+#     move(RIGHT)
