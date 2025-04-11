93eb0330e82ddd4bea7a0fbc510e0f2fc608c3ab
WingedSeal <firm09719@gmail.com>
2025-04-11 16:39:22 +0700

    feat: clear main

--- src/main.py
+++ src/main.py
@@ -2,21 +2,7 @@ from mazegame import *
 
 
 def script():
-    move(RIGHT)
-    move(DOWN)
-    move(LEFT)
-    move(UP)
-    for _ in range(3):
-        move(LEFT)
-    move(UP)
-    move(RIGHT)
-    for _ in range(3):
-        move(DOWN)
-    move(LEFT)
-    for _ in range(2):
-        move(UP)
-    for _ in range(5):
-        move(RIGHT)
+    return
 
 
 run(script, EXAMPLE1)
