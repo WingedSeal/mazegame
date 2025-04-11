faf56b71c20ffd3754f393cd86eed107c2ef6bfd
WingedSeal <firm09719@gmail.com>
2025-04-11 23:15:28 +0700

    feat: add solution to normal4

--- src/main.py
+++ src/main.py
@@ -2,7 +2,16 @@ from mazegame import *
 
 
 def script():
-    return
+    for _ in range(5):
+        wait()
+    for _ in range(5):
+        move(UP)
+    for _ in range(5):
+        move(DOWN)
+    move(RIGHT)
+    move(RIGHT)
+    for _ in range(3):
+        move(UP)
 
 
 run(script, NORMAL4)
--- src/mazegame/api/maps/normal4.py
+++ src/mazegame/api/maps/normal4.py
@@ -9,7 +9,7 @@ DOOR1 = Door(color1)
 DOOR2 = Door(color2, open=True)
 ENEMY1 = Enemy([Direction.UP] * 5 + [Direction.DOWN] * 7 + [Direction.UP] * 2)
 ENEMY2 = Enemy(
-    [Direction.HALT] * 12 + [Direction.DOWN] + [Direction.HALT] * 10 + [Direction.UP]
+    [Direction.HALT] * 12 + [Direction.DOWN] + [Direction.HALT] * 12 + [Direction.UP]
 )
 # fmt: off
 NORMAL4 = Map(
@@ -26,3 +26,15 @@ NORMAL4 = Map(
     ]
 )
 # fmt: on
+
+# def script():
+#     for _ in range(5):
+#         wait()
+#     for _ in range(5):
+#         move(UP)
+#     for _ in range(5):
+#         move(DOWN)
+#     move(RIGHT)
+#     move(RIGHT)
+#     for _ in range(3):
+#         move(UP)
