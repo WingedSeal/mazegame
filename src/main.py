151e63349aff382144a5f5df60093839bd508308
WingedSeal <firm09719@gmail.com>
2025-04-14 19:33:06 +0700

    feat: add solution for practice1

--- src/main.py
+++ src/main.py
@@ -2,8 +2,25 @@ from mazegame import *
 
 
 def script():
-    pass
+    move(RIGHT)
+    move(DOWN)
+    move(DOWN)
+    for _ in range(4):
+        move(LEFT)
+    for _ in range(4):
+        move(UP)
+    for _ in range(4):
+        move(RIGHT)
+    move(UP)
+    move(RIGHT)
+    move(UP)
+    move(LEFT)
+    move(DOWN)
+    for _ in range(3):
+        move(LEFT)
+    move(UP)
+    move(UP)
 
 
-# run(script, NORMAL1)
-preview(PRACTICE1)
+run(script, PRACTICE1)
+# preview(PRACTICE1)
--- src/mazegame/api/maps/practice1.py
+++ src/mazegame/api/maps/practice1.py
@@ -26,3 +26,23 @@ XXXXXXXX
     )
 )
 PRACTICE1 = ([PRACTICE1_1], "There are 2 players")
+
+# def script():
+#     move(RIGHT)
+#     move(DOWN)
+#     move(DOWN)
+#     for _ in range(4):
+#         move(LEFT)
+#     for _ in range(4):
+#         move(UP)
+#     for _ in range(4):
+#         move(RIGHT)
+#     move(UP)
+#     move(RIGHT)
+#     move(UP)
+#     move(LEFT)
+#     move(DOWN)
+#     for _ in range(3):
+#         move(LEFT)
+#     move(UP)
+#     move(UP)
