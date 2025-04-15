a2d86fd4fab284069d75bcfb49f908ab53a0d2e3
WingedSeal <firm09719@gmail.com>
2025-04-15 21:01:26 +0700

    typo: add ( to tutorial2 desc)

--- src/main.py
+++ src/main.py
@@ -2,33 +2,8 @@ from mazegame import *
 
 
 def script():
-    move(DOWN)
-    move(RIGHT)
-    move(RIGHT)
-    move(UP)
-    move(UP)
-    move(UP)
-    move(DOWN)
-    move(RIGHT)
-    move(RIGHT)
-    move(UP)
-    move(DOWN)
-    move(LEFT)
-
-    # move(DOWN)
-    # move(RIGHT)
-    # move(RIGHT)
-    # move(UP)
-    # move(UP)
-    # move(RIGHT)
-    # move(RIGHT)
-    # move(RIGHT)
-    # move(DOWN)
-    # move(DOWN)
-    # move(LEFT)
-    # move(LEFT)
-    # move(UP)
+    pass
 
 
-run(script, PRACTICE2)
-# preview(PRACTICE2)
+run(script, TUTORIAL1)
+# preview(TUTORIAL1)
